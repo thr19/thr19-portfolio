9442563a6aba2d84a9ccad7a0f00858f132c96e7
thr19 <thr19@vivaldi.net>
2025-06-20 16:11:11 +0400

    bootsrap-icon: Add Bootstrap icons to the project

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 import Alpine from 'alpinejs'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 window.Alpine = Alpine
 Alpine.start()
